8268a55257d083607179fe1be56c0dc1196df895
Sanduni Wijethunga <sanduni93193@gmail.com>
2019-06-12 09:25:51 +0530

    errors handle

--- src/component/customer/Footer.js
+++ src/component/customer/Footer.js
@@ -6,58 +6,59 @@ class Footer extends Component{
         return(
             <footer id="footcustm">
                 <div className="inner">
-                <div className="row">
-                <div className="col-sm-3">
-                    <h5>What Can Do</h5>
-                    <br></br>
-                    Book Tour Package
-                <br></br>
-               Book Vehicle
-                <br></br>
-                Book Hotel
-                <br></br>
-               Create Your Tour
-                </div>
+                    <div className="row">
+                        <div className="col-sm-3">
+                            <h5>What Can Do</h5>
+                            <br></br>
+                            Book Tour Package
+                            <br></br>
+                            Book Vehicle
+                            <br></br>
+                            Book Hotel
+                            <br></br>
+                            Create Your Tour
+                        </div>
                 
-                <div className="col-sm-3">
-                    <h5>Contact Us</h5>
-                    <br></br>
-                    Mobile: +112367874
-                <br></br>
-                E-mail: gotrip.lk@gmail.com
-                <br></br>
-                Fb: GoTrip
-                <br></br>
-                Insta: GoTrip34
-                </div>
-                <div className="col-sm-3">
-                    <h5>Where We In</h5>
-                    <br></br>
-                    116, Script Complex,
-                <br></br>
-               Nawala Road,
-                <br></br>
-                Nugegoda,
-                <br></br>
-                Sri Lanka
-                </div>
+                        <div className="col-sm-3">
+                            <h5>Contact Us</h5>
+                            <br></br>
+                            Mobile: +112367874
+                            <br></br>
+                            E-mail: gotrip.lk@gmail.com
+                            <br></br>
+                            Fb: GoTrip
+                            <br></br>
+                            Insta: GoTrip34
+                        </div>
+                        <div className="col-sm-3">
+                            <h5>Where We In</h5>
+                            <br></br>
+                            116, Script Complex,
+                            <br></br>
+                            Nawala Road,
+                            <br></br>
+                            Nugegoda,
+                            <br></br>
+                            Sri Lanka
+                        </div>
 
                 <div className="col-sm-3">
-                    <h5>Our Team</h5>
-                    <br></br>
-                    Sanduni Dilhara
-                <br></br>
-               Asangi Sathsarani
-                <br></br>
-                Gaweshi Dias
-                <br></br>
-                Nadeesha Shihani
+                        <h5>Our Team</h5>
+                        <br></br>
+                        Sanduni Dilhara
+                        <br></br>
+                        Asangi Sathsarani
+                        <br></br>
+                        Gaweshi Dias
+                        <br></br>
+                        Nadeesha Shihani
                 </div>
                     
                 <div>
-                <br></br>
-                <br></br>
-                <h6 className="text-center"> © 2019 All Rights Reserved by GoTrip Team.</h6>
+                    <br></br>
+                    <br></br>
+                    <h6 className="text-center"> © 2019 All Rights Reserved by GoTrip Team.</h6>
+                </div>
                 </div>
                 </div>
             </footer>
